3f5245e66173246f8e6bb4707a16dc6f7283e5b7
kyledexperience <kyledexperience@gmail.com>
2024-11-05 17:56:42 -0500

    functions

--- godemo.go
+++ godemo.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+func concat(s1, s2 string) string {
+	return s1 + s2
+}
+
+func sum(n1, n2 int) int {
+	return n1 + n2
+}
+
+func increment(x int) int {
+	x = x + 1
+	return x
+}
+
 func main() {
 
 	messageLen := 10
@@ -13,5 +26,12 @@ func main() {
 	} else {
 		fmt.Println("Message too long")
 	}
+
+	fmt.Println(concat("Bob", " The Builder"))
+	fmt.Println("2 + 3 = ", sum(2, 3))
+
+	x := 1
+	x = increment(x)
+	fmt.Println(x)
 
 }
