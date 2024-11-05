6406e0457fc774d153235b17379bcaa0b4225a3c
kyledexperience <kyledexperience@gmail.com>
2024-11-05 17:36:40 -0500

    conditionals

--- godemo.go
+++ godemo.go
@@ -4,6 +4,14 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("Hell world")
+	messageLen := 10
+
+	fmt.Println("Trying to send message of length: ", messageLen)
+
+	if maxMessageLen := 20; maxMessageLen > messageLen {
+		fmt.Println("Message Sent")
+	} else {
+		fmt.Println("Message too long")
+	}
 
 }
